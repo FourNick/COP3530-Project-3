98e9faa2647869f18d6c6afa828718d675889a86
crugenstein <103900397+crugenstein@users.noreply.github.com>
2022-04-19 21:27:11 -0400

    Create RedBlackTree.h

---
+++ RedBlackTree.h
@@ -0,0 +1,38 @@
+#include "NGO.h"
+#include <vector>
+using namespace std;
+
+
+struct Node {
+
+    unsigned long int val; // node's value (income belonging to all nonprofits)
+    Node* left;
+    Node* right;
+    Node* parent;
+    bool isRed = false; // false if black, true if red
+    vector<NGO> nonprofits; // vector storing all nonprofits that belong to unique profit node
+
+};
+
+class RedBlackTree {
+
+private:
+    Node* head; // head of red-black tree
+    int size; // number of unique NODES, not number of nonprofits (multiple nonprofits can belong to same node)
+
+public:
+    RedBlackTree(); // constructor
+    Node* getHead(); // get head of red-black tree
+    void insert(NGO toBeInserted); // insert nonprofit
+    void insertHelper(Node* node); // insertion helper
+    void rotateRight(Node* node); // right rotation helper
+    void rotateLeft(Node* node); // left rotation helper
+    bool getRed(Node* node); //returns true if node is red, false if black
+    Node* getGrandparent(Node* node); // get grandparent of node
+    Node* getUncle(Node* node); // get uncle of node
+    bool isLeftChild(Node* node); // returns true if node is a left child, false if it's a right child
+    vector<NGO> search(string term); // search by name
+    vector<NGO> flatten(); // convert all nonprofits into sorted vector
+    void flattenHelper(Node* node, vector<NGO>& vect); // helper for flatten
+
+};
