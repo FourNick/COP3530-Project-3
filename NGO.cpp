5bf57958cc4172c6fe60e512739e08b75e4f7f0d
FourNick <103899956+FourNick@users.noreply.github.com>
2022-04-19 20:27:41 -0400

    Add files via upload

--- NGO.cpp
+++ NGO.cpp
@@ -5,14 +5,17 @@ NGO::NGO(string inLine) {
 	vector<string> row;
 	stringstream rowString(inLine);
 	string word;
+
 	while (getline(rowString, word, ','))
 	{
 		row.push_back(word);
 	}
 	EIN = stoi(row[0]);
 	name = row[1];
+	state = row[5];
 	zip = row[6];
 	incomeCode = stoi(row[15]);
+	address = row[4] + ", " + row[5] + ", " + row[6] + ", " + row[7];
 	if (incomeCode != 0 && row[25] != "") {
 		income = stoll(row[25], nullptr, 10);
 	}
@@ -25,6 +28,12 @@ NGO::NGO(string name_, string zip_, long int income_) {
 	name = name_;
 	zip = zip_;
 	income = income_;
+}
+
+NGO::NGO() {
+	name = "";
+	zip = "";
+	income = 0;
 }
 
 void NGO::printName() {
@@ -41,4 +50,12 @@ long int NGO::getIncome() {
 
 string NGO::getName() {
 	return name;
+}
+
+string NGO::getAddress() {
+	return address;
+}
+
+string NGO::getState() {
+	return state;
 }
