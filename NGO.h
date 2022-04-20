624c3b77cadbf3865b669c93bd0ba5ecae32a750
FourNick <103899956+FourNick@users.noreply.github.com>
2022-04-19 20:27:56 -0400

    Add files via upload

--- NGO.h
+++ NGO.h
@@ -12,13 +12,17 @@ class NGO
 	int incomeCode;
 	long int income;
 	string zip;
+	string address;	
+	string state;
 public: 
+	NGO(); // NGO base case
 	NGO(string inLine); // constructor for import from csv
 	NGO(string name_, string zip_, long int income_); // constructor for user added NGO 
 	void printName(); // prints the name of the nonprofit formatted with a comma and space afterwards 
 	string getZip(); // returns zip code as a string
 	long int getIncome(); // returns income as a long int
 	string getName(); // returns name as a string (in all caps)
-
+	string getAddress();
+	string getState();
 };
 
