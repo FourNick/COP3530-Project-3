c08365cefb5641c4d234be96adb0900d7fca6992
BryanTorreblanca00 <80721275+BryanTorreblanca00@users.noreply.github.com>
2022-04-20 21:01:21 -0400

    Update HashMap.h

--- HashMap.h
+++ HashMap.h
@@ -7,12 +7,16 @@ using namespace std;
 class HashMap {
 
 private:
+	// Initialize tableSize to 10 of the hash map.
 	unsigned int tableSize = 10;
+	// maxLoadFactor of the table that if met, rehashing should take place.
 	float maxLoadFactor = 0.75;
 	float elements = 0;
+	// Table declaration. 
 	vector<NGO>* table;
 
 public:
+	// Where all the functions used are being declared.
 	HashMap();
 	void insert(NGO toBeInserted);
 	int hash(long unsigned int income);
